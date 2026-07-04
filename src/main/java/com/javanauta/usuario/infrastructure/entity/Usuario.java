a1ddcccc8721bccfefdc28bb0012aa0c4c694d64
Fernando Torres <nandotorresbrasil@gmail.com>
2026-07-04 02:06:06 -0300

    fix: adicionando suporte a banco de testes H2 na branch security

--- src/main/java/com/javanauta/usuario/infrastructure/entity/Usuario.java
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Usuario.java
@@ -3,10 +3,7 @@
 package com.javanauta.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -19,6 +16,7 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "usuario")
+@Data
 
 
 public class Usuario implements UserDetails {
